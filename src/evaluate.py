819ce3a84fe70d7ecb841e29adad9327f288d561
khuyentran1401 <khuyentran1476@gmail.com>
2023-04-11 11:00:41 -0500

    Change workflow

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,24 +2,26 @@ import dvc.api
 
 from helper import load_data
 from sklearn.metrics import accuracy_score
+from dvclive import Live
 from mlem.api import load
 
 
 def load_model(path: str):
     """Load model from path"""
-    service = load(path)
-    return service.get_client()
+    return load(path)
 
 
 def evaluate() -> None:
     """Evaluate model and log metrics"""
     params = dvc.api.params_show()
-    X_test = load_data(f"{params['data']['intermediate']}/X_test.pkl")
-    y_test = load_data(f"{params['data']['intermediate']}/y_test.pkl")
-    model = load_model(params["app"])
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print(f"The model's accuracy is {accuracy}")
+    with Live(save_dvc_exp=True, resume=True) as live:
+        X_test = load_data(f"{params['data']['intermediate']}/X_test.pkl")
+        y_test = load_data(f"{params['data']['intermediate']}/y_test.pkl")
+        model = load_model(params["model"])
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        print(f"The model's accuracy is {accuracy}")
+        live.log_metric("accuracy", accuracy)
 
 if __name__ == "__main__":
     evaluate()
--- src/train.py
+++ src/train.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-
+from dvclive import Live
 from helper import load_data
 from mlem.api import save
 
@@ -38,17 +38,19 @@ def save_model(model, path: str, data: pd.DataFrame):
 def train() -> None:
     """Train model and save it"""
     params = dvc.api.params_show()
-    X_train = load_data(f"{params['data']['intermediate']}/X_train.pkl")
-    y_train = load_data(f"{params['data']['intermediate']}/y_train.pkl")
-    pipeline = create_pipeline()
-    grid_search = train_model(
-        X_train,
-        y_train,
-        pipeline,
-        params["train"]["hyperparameters"],
-        params["train"]["grid_search"],
-    )
-    save_model(grid_search.best_estimator_, params["model"], X_train)
+    with Live(save_dvc_exp=True) as live:
+        X_train = load_data(f"{params['data']['intermediate']}/X_train.pkl")
+        y_train = load_data(f"{params['data']['intermediate']}/y_train.pkl")
+        pipeline = create_pipeline()
+        grid_search = train_model(
+            X_train,
+            y_train,
+            pipeline,
+            params["train"]["hyperparameters"],
+            params["train"]["grid_search"],
+        )
+        live.log_params({"Best hyperparameters": grid_search.best_params_})
+        save_model(grid_search.best_estimator_, params["model"], X_train)
 
 
 if __name__ == "__main__":
